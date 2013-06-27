fb414425806360f6ea2475f3ee6e921fe5487af5
Rex Lorenzo <rex@oid.ucla.edu>
2013-06-27 13:22:19 -0700

    Adding page to simulate being denied access to the MyUCLA web service.

---
+++ accessdenied.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Simulates the error message that you would get from the MyUCLA web service if
+ * you are trying to access it from a non-approved IP address.
+ */
+?>
+<HTML>
+<BODY>
+
+
+		Status:  Unauthorized Access!
+
+</BODY>
+</HTML>
